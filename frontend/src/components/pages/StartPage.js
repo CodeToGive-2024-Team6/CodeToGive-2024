b2fad191e918948602c6ee94948466b055f4f347
Chloe Kim <chloekim@Chloes-MacBook-Air.local>
2024-03-23 17:03:35 -0600

    start page update

--- frontend/src/components/pages/StartPage.js
+++ frontend/src/components/pages/StartPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './StartPage.css'; // Import CSS file
 import companyLogo from './company_logo.png'; // Import company logo image
+import houseLogo from './house_logo.png'; // Import house logo image
 
 const StartPage = () => {
     return (
@@ -9,7 +10,7 @@ const StartPage = () => {
             <div className="header">
                 <img src={companyLogo} alt="Company Logo" className="logo" />
             </div>
-            
+
             <div className="button-container">
                 <Link to="/caregiver_homepage" className="button-link">
                     <button className="circle caregiver">Caregivers</button>
@@ -17,7 +18,9 @@ const StartPage = () => {
                 <Link to="/resident_homepage" className="button-link">
                     <button className="circle resident">Residents</button>
                 </Link>
+                <img src={houseLogo} alt="House Logo" className="houselogo" />
             </div>
+            <div className="rectangle"></div>
         </div>
     );
 };
