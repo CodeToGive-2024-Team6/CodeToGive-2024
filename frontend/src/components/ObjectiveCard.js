3084d3688a27849bffff7eb780ff0b76a8556be2
Jared Drueco <drueco03@gmail.com>
2024-03-24 02:22:12 -0600

    feat: working reusable side bar

--- frontend/src/components/ObjectiveCard.js
+++ frontend/src/components/ObjectiveCard.js
@@ -5,10 +5,10 @@ const ObjectiveCard = ({ objective }) => {
 
   switch (objective.status) {
     case 'Completed':
-      bgColorClass = 'bg-sky-400';
+      bgColorClass = 'bg-rose-400';
       break;
     case 'In progress':
-      bgColorClass = 'bg-sky-300';
+      bgColorClass = 'bg-rose-300';
       break;
     default:
       bgColorClass = 'bg-gray-300';
@@ -18,10 +18,14 @@ const ObjectiveCard = ({ objective }) => {
     <div
       className={`relative flex flex-col w-full ${bgColorClass} text-white text-lg rounded-2xl p-6 gap-y-2`}
     >
-      {objective.title && <h2>Title: {objective.title}</h2>}
-      {objective.status && <p>Status: {objective.status}</p>}
-      {objective.term && <p>Term: {objective.term}</p>}
-      {objective.healthAspects && <p>Tags: {objective.healthAspects.join(', ')}</p>}
+      {objective.title && (
+        <div className="text-xl font-bold">Title: {objective.title}</div>
+      )}
+      {objective.status && <div>Status: {objective.status}</div>}
+      {objective.term && <div>Term: {objective.term}</div>}
+      {objective.healthAspects && (
+        <div>Tags: {objective.healthAspects.join(', ')}</div>
+      )}
 
       <div className="md:absolute bottom-4 right-4 bg-white hover:bg-gray-300 text-sky-400 py-2 px-4 rounded-lg cursor-pointer">
         SEE MORE
---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,99 @@
+import React, { useContext, createContext, useState } from 'react';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
+
+const SidebarContext = createContext();
+
+const SideBar = ({ children, onActivateItem }) => {
+  const [expanded, setExpanded] = useState(true);
+  const [activeItem, setActiveItem] = useState('GOALS');
+
+  const activateItem = (itemName) => {
+    setActiveItem(itemName);
+    if (onActivateItem) {
+      onActivateItem(itemName);
+    }
+  };
+
+  return (
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-white border-r shadow-lg">
+        <div className="p-4 pb-2 flex justify-between items-center mb-16">
+          <img
+            src="/images/company_logo1.png"
+            className={`overflow-hidden transition-all ${
+              expanded ? 'w-32' : 'w-0'
+            }`}
+            alt=""
+          />
+          <div
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+          >
+            {expanded ? <ChevronLeft /> : <ChevronRight />}
+          </div>
+        </div>
+
+        <SidebarContext.Provider value={{ expanded, activateItem }}>
+          <ul className="flex-1 px-3 flex flex-col gap-y-6">
+            {React.Children.map(children, (child) =>
+              React.cloneElement(child, {
+                active: activeItem === child.props.text,
+              })
+            )}
+          </ul>
+        </SidebarContext.Provider>
+      </nav>
+    </aside>
+  );
+};
+
+const SideBarItem = ({ icon, text, active, alert }) => {
+  const { expanded, activateItem } = useContext(SidebarContext);
+
+  const handleClick = () => {
+    activateItem(text);
+  };
+
+  return (
+    <li
+      onClick={handleClick}
+      className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${active ? 'bg-pink-100' : 'hover:bg-pink-50 text-gray-600'}
+    `}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all ${
+          expanded ? 'w-52 ml-3' : 'w-0'
+        }`}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-rose-400 ${
+            expanded ? '' : 'top-2'
+          }`}
+        />
+      )}
+
+      {!expanded && (
+        <div
+          className={`
+          absolute left-full rounded-md px-2 py-1 ml-6
+          bg-indigo-100 text-indigo-800 text-sm
+          invisible opacity-20 -translate-x-3 transition-all
+          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      `}
+        >
+          {text}
+        </div>
+      )}
+    </li>
+  );
+};
+
+export { SideBar, SideBarItem };
