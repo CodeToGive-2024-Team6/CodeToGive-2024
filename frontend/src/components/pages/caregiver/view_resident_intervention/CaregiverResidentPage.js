98eab994f665e0d80b21328526890d45bfd7408d
Mira <mirapatel2512@gmail.com>
2024-03-23 23:03:52 -0600

    update resident intervention view to incorporate navigation across the 6 tabs upon a user's click.

--- frontend/src/components/pages/caregiver/view_resident_intervention/CaregiverResidentPage.js
+++ frontend/src/components/pages/caregiver/view_resident_intervention/CaregiverResidentPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./CaregiverResidentPage.css"; // Import CSS file
 
 //import company logo
@@ -15,6 +15,8 @@ import { CgNotes } from "react-icons/cg";
 
 
 const CaregiverResidentPage = () => {
+
+    const [activeContent, setActiveContent] = useState('goals');
 
     useEffect(() => {
         const list = document.querySelectorAll('.list');
@@ -38,52 +40,52 @@ const CaregiverResidentPage = () => {
         
         <div className="resident-profile-container-caregiver ">
 
-    <div class="navigation">
+            <div class="navigation">
                 <ul>
                     <VscAccount 
                         style={{color: 'white', fontSize: '1em', marginRight: '50px'}}
                     />
                     {/* Main content of your application */}
 
-                    <li class="list active">
-                        <a href="#">
+                    <li className={activeContent === 'profile' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('profile')}>
                             <span class="icon">
                             <FaRegUser style={{fontSize: '1em'}}/></span>
                             <span class="title">PROFILE</span>
                         </a>
                     </li>
 
-                    <li class="list">
-                        <a href="#">
+                    <li className={activeContent === 'goals' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('goals')}>
                             <span class="icon">
                             <LuGoal style={{fontSize: '1em'}}/></span>
                             <span class="title">GOALS</span>
                         </a>
                     </li>
-                    <li class="list">
-                        <a href="#">
+                    <li className={activeContent === 'reminders' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('reminders')}>
                             <span class="icon">
                             <FaRegBell style={{fontSize: '1em'}}/></span>
                             <span class="title">REMINDERS</span>
                         </a>
                     </li>
-                    <li class="list">
-                        <a href="#">
+                    <li className={activeContent === 'resources' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('resources')}>
                             <span class="icon">
                             <FaBook style={{fontSize: '1em'}}/></span>
                             <span class="title">RESOURCES</span>
                         </a>
                     </li>
-                    <li class="list">
-                        <a href="#">
+                    <li className={activeContent === 'messages' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('messages')}>
                             <span class="icon">
                             <FiMessageSquare style={{fontSize: '1em'}}/></span>
                             <span class="title">MESSAGES</span>
                         </a>
                     </li>
 
-                    <li class="list">
-                        <a href="#">
+                    <li  className={activeContent === 'notes' ? 'list active' : 'list'}>
+                        <a href="#" onClick={() => setActiveContent('notes')}>
                             <span class="icon">
                             <CgNotes style={{fontSize: '1em'}}/></span>
                             <span class="title">NOTES</span>
@@ -103,11 +105,49 @@ const CaregiverResidentPage = () => {
     
             </div>
 
-
+            {/* Additional content for the Caregiver Resident Page */}
+            <div className='additional-content'>
+                {activeContent === 'profile' && (
+                    <>
+                        <h2>Welcome Back, (Caregiver Name) 👋</h2>
+                        <p>Your profile information will be displayed here!</p>
+                    </>
+                )}
+                {activeContent === 'goals' && (
+                    <>
+                        <h2>Goals</h2>
+                        <p>Your SMART goals/objectives forms and list will be displayed on this page!</p>
+                    </>
+                )}
+                {activeContent === 'reminders' && (
+                    <>
+                        <h2>Reminders</h2>
+                        <p>Your reminders will be displayed here!</p>
+                    </>
+                )}
+                {activeContent === 'resources' && (
+                    <>
+                        <h2>Resources</h2>
+                        <p>Your resources will be displayed here!</p>
+                    </>
+                )}
+                {activeContent === 'messages' && (
+                    <>
+                        <h2>Messages</h2>
+                        <p>Your messages will be tracked here!</p>
+                    </>
+                )}
+                {activeContent === 'notes' && (
+                    <>
+                        <h2>Notes</h2>
+                        <p>Your notes will be tracked here!</p>
+                    </>
+                )}
+            </div>
         
         </div>
     );
-    }
+};
 
 export default CaregiverResidentPage;
 
