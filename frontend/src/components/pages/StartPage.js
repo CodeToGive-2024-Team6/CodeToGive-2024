38ed43fbe3ffe0ca6ae8f5b2b9caeb0c05ea9122
Hacker Spoingus <130513711+ChloeNahyunKim@users.noreply.github.com>
2024-03-24 21:57:38 -0600

    Update StartPage.js

--- frontend/src/components/pages/StartPage.js
+++ frontend/src/components/pages/StartPage.js
@@ -5,7 +5,8 @@ import companyLogo from './company_logo.png'; // Import company logo image
 import houseLogo from './house_logo.png'; // Import house logo image
 import { FaPeopleRoof } from "react-icons/fa6";
 import { TbUserHeart } from "react-icons/tb";
-
+import { BiHomeHeart } from "react-icons/bi";
+import { BsHearts } from "react-icons/bs";
 
 
 const StartPage = () => {
@@ -13,27 +14,33 @@ const StartPage = () => {
         <div className="start-page">
             <div className="header">
                 <img src={companyLogo} alt="Company Logo" className="logo" />
+                <a href="https://lechainon.org/en/">
+                    <div class="description"><BiHomeHeart style={{ fontSize: '2.5em' }} /></div>
+                </a>
             </div>
+            <div className="border"><div className="quote"><h1>Shelter for all women<BsHearts />
 
-            <div className="button-container">
-                <Link to="/caregiver_homepage" className="button-link">
-                    <button className="circle caregiver">
-                        <TbUserHeart style={{ fontSize: '4em' }} />
-                    </button>
-                    <div className="caregivers-name">
-                        <p>Caregivers</p>
-                    </div>
-                </Link>
-                <Link to="/resident_homepage" className="button-link">
-                    <button className="circle resident">
-                        <FaPeopleRoof style={{ fontSize: '4em' }} />
-                    </button>
-                    <div className="residents-name">
-                        <p>Residents</p>
-                    </div>
-                </Link>
-
-                <img src={houseLogo} alt="House Logo" className="houselogo" />
+            </h1></div></div>
+            <div className="container">
+                <div className="button-container">
+                    <Link to="/caregiver_homepage" className="button-link">
+                        <button className="circle caregiver">
+                            <TbUserHeart style={{ fontSize: '4em' }} />
+                        </button>
+                        <div className="caregivers-name">
+                            <p>Caregivers</p>
+                        </div>
+                    </Link>
+                    <Link to="/resident_homepage" className="button-link">
+                        <button className="circle resident">
+                            <FaPeopleRoof style={{ fontSize: '4em' }} />
+                        </button>
+                        <div className="residents-name">
+                            <p>Residents</p>
+                        </div>
+                    </Link>
+                    <img src={houseLogo} alt="House Logo" className="houselogo" />
+                </div>
             </div>
             <div className="rectangle"></div>
             <div className="heart-container">
