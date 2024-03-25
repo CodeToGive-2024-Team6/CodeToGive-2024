9c1996138e4712e2ea9c4dd7c9cca7b90a176d46
Mostafam12 <Mostafa71@hotmail.co.uk>
2024-03-25 00:59:02 -0600

    changed setResidentInfo slightly

--- backend/server/setResidentData.js
+++ backend/server/setResidentData.js
@@ -37,31 +37,9 @@ function setFollowUps(residentId, followUpsData) {
 //JSON To be structured as follows:
 // {age: integer, borough: string, caregivers: array[caregiver-IDS], challenges: array[string], currentAccomodation: string, endOfStayDate: date, firstName: string, lastName: string, firstVisit: boolean, immigrationStatus: string, income: string, native: boolean, planEndDate: date, planStartDate: date, significantPersons: array[string], veteran: boolean, withChild: boolean}
 function setResidentInfo(generalInfoData) {
-    const generalInfoRef = db.collection('residents').doc(generalInfoData.residentID);
-    return generalInfoRef.set({
-      firstName: generalInfoData.firstName,
-      lastName: generalInfoData.lastName,
-      planStartDate: generalInfoData.planStartDate,
-      startDateOfStay: generalInfoData.startDateOfStay,
-      endOfStayDate: generalInfoData.endOfStayDate,
-      placeOfAccommodation: generalInfoData.placeOfAccommodation,
-      firstVisit: generalInfoData.firstVisit,
-      immigrationStatus: generalInfoData.immigrationStatus,
-      native: generalInfoData.native,
-      veteran: generalInfoData.veteran,
-      withChild: generalInfoData.withChild,
-      exitOrientation: generalInfoData.exitOrientation,
-      issues: generalInfoData.issues,
-      age: generalInfoData.age,
-      borough: generalInfoData.borough,
-      revenue: generalInfoData.revenue,
-      caregivers: generalInfoData.caregivers,
-      significantPersons: generalInfoData.significantPersons,
-      treatmentTeam: generalInfoData.treatmentTeam,
-      communityServices: generalInfoData.communityServices
-      
-    });
-  }
+    const generalInfoRef = db.collection('residents').doc(); 
+    return generalInfoRef.set(generalInfoData);
+}
 
 module.exports = {
     
