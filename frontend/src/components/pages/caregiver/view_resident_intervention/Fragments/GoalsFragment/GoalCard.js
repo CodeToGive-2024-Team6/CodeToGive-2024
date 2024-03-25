26cb48637a3d2610d29cf0133e76f238efcc65ba
Jared Drueco <drueco03@gmail.com>
2024-03-25 00:00:15 -0600

    feat: working add / edit for goals

--- frontend/src/components/pages/caregiver/view_resident_intervention/Fragments/GoalsFragment/GoalCard.js
+++ frontend/src/components/pages/caregiver/view_resident_intervention/Fragments/GoalsFragment/GoalCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import GoalModal from './GoalModal'; // Import the new component
+import GoalModal from './GoalModal';
 
-const GoalCard = ({ objective }) => {
+const GoalCard = ({ goal, onEditGoal }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -15,7 +15,7 @@ const GoalCard = ({ objective }) => {
 
   // Define the class based on objective status
   let bgColorClass = '';
-  switch (objective.status) {
+  switch (goal.status) {
     case 'Completed':
       bgColorClass = 'bg-rose-400';
       break;
@@ -28,16 +28,31 @@ const GoalCard = ({ objective }) => {
 
   return (
     <>
-      <div className={`relative flex flex-col w-full ${bgColorClass} text-white text-lg rounded-2xl p-6 gap-y-2`}>
-        {objective.title && <div className="text-xl font-bold">Title: {objective.title}</div>}
-        {objective.status && <div>Status: {objective.status}</div>}
-        {objective.term && <div>Term: {objective.term}</div>}
-        {objective.healthAspects && <div>Tags: {objective.healthAspects.join(', ')}</div>}
-        <div onClick={() => setIsModalOpen(true)} className="md:absolute bottom-4 right-4 bg-white hover:bg-gray-300 text-sky-400 py-2 px-4 rounded-lg cursor-pointer">
+      <div
+        className={`relative flex flex-col w-full ${bgColorClass} text-white text-lg rounded-2xl p-6 gap-y-2`}
+      >
+        {goal.title && (
+          <div className="text-xl font-bold">Title: {goal.title}</div>
+        )}
+        {goal.status && <div>Status: {goal.status}</div>}
+        {goal.term && <div>Term: {goal.term}</div>}
+        {goal.healthAspects && (
+          <div>Tags: {goal.healthAspects.join(', ')}</div>
+        )}
+        <div
+          onClick={() => setIsModalOpen(true)}
+          className="md:absolute bottom-4 right-4 bg-white hover:bg-gray-300 text-sky-400 py-2 px-4 rounded-lg cursor-pointer"
+        >
           EDIT
         </div>
       </div>
-      <GoalModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} objective={objective} mode="edit" />
+      <GoalModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        goal={goal}
+        onAddEditGoal={onEditGoal}
+        mode={"edit"}
+      />
     </>
   );
 };
--- frontend/src/components/pages/resident/Fragments/GoalsFragment/GoalsFragment.js
+++ frontend/src/components/pages/resident/Fragments/GoalsFragment/GoalsFragment.js
@@ -3,28 +3,28 @@ import axios from 'axios';
 import GoalCard from './GoalCard';
 
 function GoalsFragment() {
-  const [objectives, setObjectives] = useState([]);
+  const [objectives, setGoals] = useState([]);
 
   useEffect(() => {
     const residentId = '1';
-    const fetchObjectives = async () => {
+    const fetchGoals = async () => {
       try {
         const response = await axios.get(`/objectives/${residentId}`);
-        setObjectives(response.data[0].objectives); // Assuming the data structure from your example
+        setGoals(response.data[0].objectives); // Assuming the data structure from your example
       } catch (error) {
         console.error('Failed to fetch objectives:', error);
       }
     };
 
-    fetchObjectives();
+    fetchGoals();
   }, []);
 
-  const totalObjectives = objectives.length;
-  const completedObjectives = objectives.filter(
+  const totalGoals = objectives.length;
+  const completedGoals = objectives.filter(
     (objective) => objective.status === 'Completed'
   ).length;
   const completionPercentage =
-    totalObjectives > 0 ? (completedObjectives / totalObjectives) * 100 : 0;
+    totalGoals > 0 ? (completedGoals / totalGoals) * 100 : 0;
 
   return (
     <div className="flex flex-col md:flex-row gap-x-6">
@@ -41,7 +41,7 @@ function GoalsFragment() {
           <div className="flex flex-col shadow-md rounded-2xl p-6 gap-2">
             <div className="text-lg font-medium">Your Progress</div>
             <div>
-              You've completed {completedObjectives} of {totalObjectives}{' '}
+              You've completed {completedGoals} of {totalGoals}{' '}
               objectives!
             </div>
             <div className="w-full bg-gray-200 rounded-full h-2.5 mt-3">
